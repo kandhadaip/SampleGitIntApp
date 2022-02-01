e0acfbf07075d90dc2833719dc8eccbd82c683f4
kandhadaip <kandhadaip@douglascollege.ca>
2022-02-01 12:18:43 -0800

    Added a comment from local

--- app/src/main/java/com/example/samplegitintapp/MainActivity.java
+++ app/src/main/java/com/example/samplegitintapp/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Comment added from local repo - comment 1
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
